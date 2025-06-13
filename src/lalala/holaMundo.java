b54c6c8bc4c1b90dd7e4bb7fc19dcedb1980fb57
AromOrtega <arom_ortega05@hotmail.com>
2025-06-12 20:51:43 -0500

    Cambie el archivo java hola mundo

--- src/lalala/holaMundo.java
+++ src/lalala/holaMundo.java
@@ -2,6 +2,6 @@ package lalala;
 
 public class holaMundo {
     public static void main(String[] args) {
-        System.out.println("¡Hola desde Eclipse HAOALALLALALALLAL !");
+        System.out.println("¡TE ODIO ECLIPSE BASURA DE MIERA !");
     }
 }
